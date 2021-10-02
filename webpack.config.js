29d003468f25806ac75b9aa7022ea6aca3792d97
Edgar <ecarrillo046@gmail.com>
2021-10-01 18:18:02 -0700

    Add starter files and folders

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Image Carousel",
+      template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin(),
+  ],
+  devtool: "source-map",
+};
