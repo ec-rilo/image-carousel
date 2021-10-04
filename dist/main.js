e51b3d7ecd40f345abee04db8183c39ff25e9ec3
Edgar <ecarrillo046@gmail.com>
2021-10-03 17:35:16 -0700

    update

--- dist/main.js
+++ dist/main.js
@@ -1,5 +1,4 @@
 /******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
 /***/ "./src/styles/main.css":
@@ -8,6 +7,7 @@
   \*****************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
+"use strict";
 __webpack_require__.r(__webpack_exports__);
 // extracted by mini-css-extract-plugin
 
@@ -20,6 +20,7 @@ __webpack_require__.r(__webpack_exports__);
   \******************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
+"use strict";
 __webpack_require__.r(__webpack_exports__);
 // extracted by mini-css-extract-plugin
 
@@ -30,34 +31,62 @@ __webpack_require__.r(__webpack_exports__);
 /*!***************************************!*\
   !*** ./src/modules/carousel-logic.js ***!
   \***************************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+/***/ (() => {
 
-__webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _images_carousel_imgs_img_1_jpeg__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../images/carousel-imgs/img-1.jpeg */ "./src/images/carousel-imgs/img-1.jpeg");
-/* harmony import */ var _images_carousel_imgs_img_2_jpeg__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../images/carousel-imgs/img-2.jpeg */ "./src/images/carousel-imgs/img-2.jpeg");
-/* harmony import */ var _images_carousel_imgs_img_3_jpeg__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../images/carousel-imgs/img-3.jpeg */ "./src/images/carousel-imgs/img-3.jpeg");
-/* harmony import */ var _images_carousel_imgs_img_4_jpeg__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../images/carousel-imgs/img-4.jpeg */ "./src/images/carousel-imgs/img-4.jpeg");
-/* harmony import */ var _images_carousel_imgs_img_5_jpeg__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../images/carousel-imgs/img-5.jpeg */ "./src/images/carousel-imgs/img-5.jpeg");
 // Create a function that allows a img to go left
 // create a function that allows a img to go right
 // create a function that allows the little navigation rectangles to populate depending on amount of images
 // create a function that gives the little navigtion rectangles logic
 
-
-
-
-
-
+// import img1 from "../images/carousel-imgs/img-1.jpeg";
+// import img2 from "../images/carousel-imgs/img-2.jpeg";
+// import img3 from "../images/carousel-imgs/img-3.jpeg";
+// import img4 from "../images/carousel-imgs/img-4.jpeg";
+// import img5 from "../images/carousel-imgs/img-5.jpeg";
 
 const carouselLogic = (() => {
-  class ProgramImg {
-    constructor(currImg, imgTag) {
-      this.currImg = currImg;
-      this.imgTag = imgTag;
-    }
+  const img1 = document.querySelector(".img-1");
+  const img2 = document.querySelector(".img-2");
+  const img3 = document.querySelector(".img-3");
+  const img4 = document.querySelector(".img-4");
+  const img5 = document.querySelector(".img-5");
 
-    applyToHTML() {
-      this.imgTag.setAttribute("src", `${this.currImg}`);
+  // class ProgramImg {
+  //   constructor(currImg, imgTag) {
+  //     this.currImg = currImg;
+  //     this.imgTag = imgTag;
+  //   }
+
+  //   applyToHTML() {
+  //     this.imgTag.setAttribute("src", `${this.currImg}`);
+  //   }
+
+  //   applyEnteringImgAnim() {
+  //     if (this.imgTag.classList.contains("exiting-img-anim")) {
+  //       this.imgTag.classList.remove("exiting-img-anim");
+  //     }
+  //     this.imgTag.classList.add("entering-img-anim");
+  //   }
+
+  //   applyExitingImgAnim() {
+  //     if (this.imgTag.classList.contains("entering-img-anim")) {
+  //       this.imgTag.classList.remove("entering-img-anim");
+  //     }
+  //     this.imgTag.classList.add("exiting-img-anim");
+  //   }
+
+  //   updateImg(newImg) {
+  //     this.imgTag.setAttribute("src", `${newImg}`);
+  //   }
+
+  //   getTag() {
+  //     return this.imgTag;
+  //   }
+  // }
+
+  class ProgramImg {
+    constructor(imgTag) {
+      this.imgTag = imgTag;
     }
 
     applyEnteringImgAnim() {
@@ -74,13 +103,46 @@ const carouselLogic = (() => {
       this.imgTag.classList.add("exiting-img-anim");
     }
 
-    updateImg(newImg) {
-      this.imgTag.setAttribute("src", `${newImg}`);
-    }
-
     getTag() {
       return this.imgTag;
     }
+
+    get2ndClass(element) {
+      return element && element.classList.length > 1
+        ? this.imgTag.classList[1]
+        : null;
+    }
+
+    setDefaultClasses() {
+      this.imgTag.removeAttribute("class");
+      this.imgTag.classList.add("program-img");
+    }
+
+    addClass(className) {
+      this.imgTag.classList.add(className);
+    }
+  }
+
+  function populateNavigationRects(imgArr) {
+    const navRectContainer = document.querySelector(
+      ".navigation-rectangles-container"
+    );
+    const numOfImgs = imgArr.length;
+    for (let i = 0; i < numOfImgs; ++i) {
+      const currNavRect = document.createElement("div");
+      currNavRect.classList.add("navigation-rect");
+      navRectContainer.appendChild(currNavRect);
+    }
+  }
+
+  function initNavRectLogic() {
+    const navRectContainer = document.querySelector(
+      ".navigation-rectangles-container"
+    );
+    const navRectChildren = navRectContainer.childNodes;
+    navRectChildren.forEach((rectChild) => {
+      // If the current child is clicked rotate to that image
+    });
   }
 
   function initImgRotation(imgArr) {
@@ -110,60 +172,58 @@ const carouselLogic = (() => {
       currImg = newImg;
     }, 5000);
   }
-  const imgArr = [_images_carousel_imgs_img_1_jpeg__WEBPACK_IMPORTED_MODULE_0__, _images_carousel_imgs_img_2_jpeg__WEBPACK_IMPORTED_MODULE_1__, _images_carousel_imgs_img_3_jpeg__WEBPACK_IMPORTED_MODULE_2__, _images_carousel_imgs_img_4_jpeg__WEBPACK_IMPORTED_MODULE_3__, _images_carousel_imgs_img_5_jpeg__WEBPACK_IMPORTED_MODULE_4__];
-  initImgRotation(imgArr);
+
+  function nextImgBtnLogic(imgArr) {
+    const nextBtn = document.querySelector(".right-arrow");
+    const imgContainer = document.querySelector(".img-container");
+
+    let count = 0;
+    nextBtn.addEventListener("click", () => {
+      const currImg = document.querySelector(".curr-img");
+      const currImgItem = new ProgramImg(currImg);
+      let newImg;
+      let newImgItem;
+      currImgItem.applyExitingImgAnim();
+      currImg.addEventListener("animationend", () => {
+        currImgItem.setDefaultClasses();
+        currImgItem.addClass("new-img");
+      });
+      if (count === imgArr.length - 1) {
+        newImg = imgContainer.firstChild.nextSibling;
+        newImg.classList.add("curr-img");
+        newImgItem = new ProgramImg(newImg);
+        newImgItem.applyEnteringImgAnim();
+        newImg.addEventListener("animationend", () => {
+          newImgItem.setDefaultClasses();
+          newImgItem.addClass("curr-img");
+          count = 1;
+        });
+      } else {
+        newImg = currImg.nextSibling.nextSibling;
+        newImgItem = new ProgramImg(newImg);
+        newImgItem.applyEnteringImgAnim();
+        newImg.addEventListener("animationend", () => {
+          newImgItem.setDefaultClasses();
+          newImgItem.addClass("curr-img");
+        });
+      }
+      count += 1;
+    });
+  }
+
+  function prevImgBtnLogic() {
+    // logic for the previous button that cycles to the previous image.
+  }
+
+  const imgArr = [img1, img2, img3, img4, img5];
+
+  // initImgRotation(imgArr);
+  populateNavigationRects(imgArr);
+  nextImgBtnLogic(imgArr);
+  // prevImgBtnLogic();
+  // initNavRectLogic();
 })();
 
-
-/***/ }),
-
-/***/ "./src/images/carousel-imgs/img-1.jpeg":
-/*!*********************************************!*\
-  !*** ./src/images/carousel-imgs/img-1.jpeg ***!
-  \*********************************************/
-/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
-
-module.exports = __webpack_require__.p + "7878ffd1572be54fee2e.jpeg";
-
-/***/ }),
-
-/***/ "./src/images/carousel-imgs/img-2.jpeg":
-/*!*********************************************!*\
-  !*** ./src/images/carousel-imgs/img-2.jpeg ***!
-  \*********************************************/
-/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
-
-module.exports = __webpack_require__.p + "204d8e3b938aab929abb.jpeg";
-
-/***/ }),
-
-/***/ "./src/images/carousel-imgs/img-3.jpeg":
-/*!*********************************************!*\
-  !*** ./src/images/carousel-imgs/img-3.jpeg ***!
-  \*********************************************/
-/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
-
-module.exports = __webpack_require__.p + "287cd7b32e3b21acb4a4.jpeg";
-
-/***/ }),
-
-/***/ "./src/images/carousel-imgs/img-4.jpeg":
-/*!*********************************************!*\
-  !*** ./src/images/carousel-imgs/img-4.jpeg ***!
-  \*********************************************/
-/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
-
-module.exports = __webpack_require__.p + "0acf9775477608101943.jpeg";
-
-/***/ }),
-
-/***/ "./src/images/carousel-imgs/img-5.jpeg":
-/*!*********************************************!*\
-  !*** ./src/images/carousel-imgs/img-5.jpeg ***!
-  \*********************************************/
-/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
-
-module.exports = __webpack_require__.p + "dfc675a278fe4855f7c2.jpeg";
 
 /***/ })
 
@@ -194,16 +254,33 @@ module.exports = __webpack_require__.p + "dfc675a278fe4855f7c2.jpeg";
 /******/ 	}
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/global */
+/******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
-/******/ 		__webpack_require__.g = (function() {
-/******/ 			if (typeof globalThis === 'object') return globalThis;
-/******/ 			try {
-/******/ 				return this || new Function('return this')();
-/******/ 			} catch (e) {
-/******/ 				if (typeof window === 'object') return window;
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = (module) => {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				() => (module['default']) :
+/******/ 				() => (module);
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
 /******/ 			}
-/******/ 		})();
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/make namespace object */
@@ -217,30 +294,11 @@ module.exports = __webpack_require__.p + "dfc675a278fe4855f7c2.jpeg";
 /******/ 		};
 /******/ 	})();
 /******/ 	
-/******/ 	/* webpack/runtime/publicPath */
-/******/ 	(() => {
-/******/ 		var scriptUrl;
-/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
-/******/ 		var document = __webpack_require__.g.document;
-/******/ 		if (!scriptUrl && document) {
-/******/ 			if (document.currentScript)
-/******/ 				scriptUrl = document.currentScript.src
-/******/ 			if (!scriptUrl) {
-/******/ 				var scripts = document.getElementsByTagName("script");
-/******/ 				if(scripts.length) scriptUrl = scripts[scripts.length - 1].src
-/******/ 			}
-/******/ 		}
-/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
-/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
-/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
-/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
-/******/ 		__webpack_require__.p = scriptUrl;
-/******/ 	})();
-/******/ 	
 /************************************************************************/
 var __webpack_exports__ = {};
-// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+// This entry need to be wrapped in an IIFE because it need to be in strict mode.
 (() => {
+"use strict";
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
@@ -248,6 +306,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _styles_reset_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./styles/reset.css */ "./src/styles/reset.css");
 /* harmony import */ var _styles_main_css__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./styles/main.css */ "./src/styles/main.css");
 /* harmony import */ var _modules_carousel_logic_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./modules/carousel-logic.js */ "./src/modules/carousel-logic.js");
+/* harmony import */ var _modules_carousel_logic_js__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_modules_carousel_logic_js__WEBPACK_IMPORTED_MODULE_2__);
 
 
 
